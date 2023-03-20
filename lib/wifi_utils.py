395b83b54a278488e7bd225fec87b49d437995c3
Darren Dunford <48522654+darrendunford@users.noreply.github.com>
2023-03-20 21:05:18 +0000

    wifi_connect: gracefully handle no net found

--- lib/wifi_utils.py
+++ lib/wifi_utils.py
@@ -1,6 +1,7 @@
 from wifi_secrets import networks
 import network
 import time
+from sys import exit
 
 
 def wlan_connect():
@@ -12,13 +13,17 @@ def wlan_connect():
             for net in networks:
                 wlan.connect(net['ssid'], net['psk'])
                 print(f"Waiting for connection {net['ssid']}...")
-                for _retry in range(10):
+                for _retry in range(20):
                     if wlan.isconnected():
                         break
                     time.sleep(0.5)
                 if wlan.isconnected():
                     print('Connected')
                     break
+        if not wlan.isconnected():
+            print('Unable to find any network')
+            exit(1)
+
         print(wlan.ifconfig())
         return wlan
 
