99583bb6f18ad28cf8305251f88b78fd400f9149
Darren Dunford <48522654+darrendunford@users.noreply.github.com>
2023-04-03 21:24:46 +0100

    working version with button press

--- main.py
+++ main.py
@@ -1,56 +1,73 @@
 from machine import Pin, PWM
 import lcd
 import time
-import os
-import machine
+import gc
+from urllib.urequest import urlopen
+import json
 from display_utils import read_bmp_to_buffer
-from wifi_utils import wlan_connect
+from wifi_utils import wlan_connect, wlan_disconnect
 
 BL = 13
+button = Pin(16, Pin.IN, Pin.PULL_UP)
 
 if __name__ == "__main__":
 
     wlan = wlan_connect()
 
     try:
-        # Set up the LCD
         pwm = PWM(Pin(BL))
         pwm.freq(1000)
         pwm.duty_u16(32768)  # max 65535
 
         lcd_display = lcd.LCD_1inch8()
 
-        # Display a loading screen
         lcd_display.fill(lcd_display.BLACK)
         lcd_display.text("Loading...", 2, 28, lcd_display.WHITE)
 
         lcd_display.show()
 
-        # Iterate through showing all the BMP images in the images directory
-        images = os.listdir("images")
-        while True:
-            for image_filename in sorted(images):
-                image_path = "images/" + image_filename
-                print("Loading " + image_path)
+        r = urlopen("https://d3pkoikmm0xava.cloudfront.net/list/list.json")
+        image_list = json.loads(r.read())
+        r.close()
+        print(f'Image list:\n{image_list}')
 
-                # Load the image from the file and write it to the LCD buffer
+        gc.collect()
+        print(f'Free memory: {gc.mem_free()}')
+
+        print('Downloading images')
+        for image in image_list:
+            gc.collect()
+            print(f'Reading {image["key"]}')
+            print(f'Free memory: {gc.mem_free()}')
+            r = urlopen(f'https://d3pkoikmm0xava.cloudfront.net/images/{image["key"]}')
+
+            with open(image["key"], 'wb') as fd:
+                fd.write(r.read())
+                r.close()
+            print('Done')
+
+        wlan_disconnect()
+        print('Displaying images')
+
+        while True:
+            for image in image_list:
+                print("Loading " + image["key"])
+
                 lcd_display.fill(0x0000)
-                with open(image_path, "rb") as input_stream:
+
+                with open(image["key"], "rb") as input_stream:
                     read_bmp_to_buffer(lcd_display, input_stream)
 
                 print("Loaded")
 
-                # Show the image on the display
                 lcd_display.show()
 
-                # Wait a bit before trying to load the next image. Actual time till next image shows is a
-                # few seconds more due to time to load next image from flash storage.
-                sensor_temp = machine.ADC(4)
-                conversion_factor = 3.3 / 65535
-                reading = (sensor_temp.read_u16() * conversion_factor)
-                temperature = 27 - (reading - 0.706)/0.001721
-                print("Temperature "+str(temperature))
-                time.sleep(5)
+                now = time.time()
+                flag = False
+                while time.time() < now + 2:
+                    if not flag and not button.value():
+                        print("Button Pressed")
+                        flag = True
 
     except KeyboardInterrupt as e:
         wlan_disconnect()
